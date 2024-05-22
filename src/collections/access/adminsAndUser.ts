c0ad1b5573bca96af529ae1ec49496b8578f0885
tarik razine <tarikrazine.com@gmail.com>
2024-05-22 08:16:13 +0100

    feat: any

--- src/collections/access/adminsAndUser.ts
+++ src/collections/access/adminsAndUser.ts
@@ -1,8 +1,8 @@
-import type { AccessArgs } from "payload/config";
+import type { AccessArgs, AccessResult } from "payload/config";
 
 import { checkRole } from "./checkRole";
 
-const adminsAndUser = ({ req: { user } }: AccessArgs) => {
+const adminsAndUser = ({ req: { user } }: AccessArgs): any => {
   if (user) {
     if (checkRole(["admin"], user)) {
       return true;
